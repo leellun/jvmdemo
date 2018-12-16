8bda4504449ac2cc174cc20bd81d80f2e382516c
liulun <289747235@qq.com>
2018-12-16 17:50:03 +0800

    垃圾回收器搭配测试

--- src/com/newland/jvm/GCTest.java
+++ src/com/newland/jvm/GCTest.java
@@ -6,12 +6,13 @@ import java.util.List;
 
 public class GCTest {
 	
-	//默认收集器Parallel Scavenge      PS Scavenge/PS MarkSweep
+	//默认收集器Parallel Scavenge  -XX:+UseParallelGC -XX:+UseParallelOldGC     PS Scavenge/PS MarkSweep
 	//CMS垃圾收集器   -XX:+UseConcMarkSweepGC  ParNew/ConcurrentMarkSweep
 	//Serial垃圾收集器  -XX:+UseSerialGC Copy/MarkSweepCompact
-	//ParNew垃圾收集器 -XX:+UseParNewGC ParNew/MarkSweepCompact
+	//ParNew垃圾收集器 -XX:+UseParNewGC ParNew/MarkSweepCompact     jdk1.8已经弃用
 	//G1垃圾收集器  -XX:+UseG1GC   G1 Young Generation/G1 Old Generation
 	
+	//总结：经过测试ParNew 可以搭配CMS和Serial Old垃圾收集器
 	public static void main(String[] args) {
 		List<GarbageCollectorMXBean> lists=ManagementFactory.getGarbageCollectorMXBeans();
 		for(GarbageCollectorMXBean bean:lists){
